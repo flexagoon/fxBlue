911ecff3180af36acea2c47f84fb2b8f21d8e648
Pavel Zolotarevskiy <code@fxgn.dev>
2023-12-16 09:22:25 +0000

    add nautilus-open-any-terminal

---
+++ config/scripts/nautilus-open-any-terminal.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+echo "--> Installing dependencies"
+rpm-ostree install gettext nautilus-python
+echo "--> Downloading nautilus-open-any-terminal"
+git clone https://github.com/Stunkymonkey/nautilus-open-any-terminal
+echo "--> Installing nautilus-open-any-terminal"
+cd nautilus-open-any-terminal
+chmod +x ./tools/update-extension-system.sh
+./tools/update-extension-system.sh install
+echo "--> Removing gettext build dependency"
+rpm-ostree override remove gettext
