1a533b2b31e47b7df315f5de1c8b0b73d7265e65
flexagoon <code@fxgn.dev>
2023-12-16 14:16:03 +0300

    don't hide loupe icon for now

--- config/scripts/hide-app-icons.sh
+++ config/scripts/hide-app-icons.sh
@@ -3,5 +3,4 @@
 set -oue pipefail
 
 cd /usr/share/applications
-cp /var/lib/flatpak/app/org.gnome.Loupe/current/active/export/share/applications/org.gnome.Loupe.desktop .
-echo "Hidden=true" | tee -a nvidia-settings.desktop fish.desktop org.gnome.Loupe.desktop
+echo "Hidden=true" | tee -a nvidia-settings.desktop fish.desktop
