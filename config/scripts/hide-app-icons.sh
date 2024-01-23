f21b700e272aa37b2dbc802c3cc377c0572e9c12
flexagoon <code@fxgn.dev>
2024-01-23 20:00:50 +0300

    hide the nvim desktop entry

--- config/scripts/hide-app-icons.sh
+++ config/scripts/hide-app-icons.sh
@@ -3,4 +3,4 @@
 set -oue pipefail
 
 cd /usr/share/applications
-echo "Hidden=true" | tee -a nvidia-settings.desktop fish.desktop
+echo "Hidden=true" | tee -a nvidia-settings.desktop fish.desktop nvim.desktop
