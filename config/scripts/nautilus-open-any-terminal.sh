a6cf1342854d78596c74f1d3ecaa5abe011f2e24
flexagoon <code@fxgn.dev>
2024-02-09 16:02:49 +0300

    fix nautilus-open-any-terminal installation

--- config/scripts/nautilus-open-any-terminal.sh
+++ config/scripts/nautilus-open-any-terminal.sh
@@ -8,5 +8,5 @@ echo "--> Downloading nautilus-open-any-terminal"
 git clone https://github.com/Stunkymonkey/nautilus-open-any-terminal
 echo "--> Installing nautilus-open-any-terminal"
 cd nautilus-open-any-terminal
-chmod +x ./tools/update-extension-system.sh
-./tools/update-extension-system.sh install
+make
+sudo make install schema
