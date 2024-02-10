c35295d92a8a98c62aa3f4caeb835e1be5372d2b
flexagoon <code@fxgn.dev>
2024-02-10 21:19:51 +0300

    add make build dep for NOAT

--- config/scripts/nautilus-open-any-terminal.sh
+++ config/scripts/nautilus-open-any-terminal.sh
@@ -3,7 +3,7 @@
 set -oue pipefail
 
 echo "--> Installing dependencies"
-rpm-ostree install gettext nautilus-python
+rpm-ostree install make gettext nautilus-python
 echo "--> Downloading nautilus-open-any-terminal"
 git clone https://github.com/Stunkymonkey/nautilus-open-any-terminal
 echo "--> Installing nautilus-open-any-terminal"
